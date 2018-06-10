93d91c6a433a25e461cf8922cd49ec294e3bd70d
cyrilwelschen <cj.welschen@gmail.com>
2018-06-10 14:55:05 +0200

    Working dropbox download into app storage.

--- app/src/main/java/com/example/cyrilwelschen/reservationen/DbDownloadManager.java
+++ app/src/main/java/com/example/cyrilwelschen/reservationen/DbDownloadManager.java
@@ -8,23 +8,24 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
-import android.widget.Toast;
+
 
 /**
  * Created by cyril on 09.06.18.
  *
  */
 
-public class DbDownloadManager {
+class DbDownloadManager {
 
-    Context context;
-    Activity activity;
+    private Context context;
+    private Activity activity;
 
     DbDownloadManager(Context _context, Activity _activity) {
         context = _context;
         activity = _activity;
         checkDiskPermission();
-        downloadFromDropBoxUrl();
+        downloadFromDropBoxUrl("https://dl.dropboxusercontent.com/s/387m0eytkoey2ii/gastro.db");
+        downloadFromDropBoxUrl("https://dl.dropboxusercontent.com/s/34h5r7nkapro2m2/version.info");
     }
 
     private void checkDiskPermission ()
@@ -34,13 +35,14 @@ public class DbDownloadManager {
         }
     }
 
-    private void downloadFromDropBoxUrl() {
+    private void downloadFromDropBoxUrl(String url) {
 
-        String url = "https://dl.dropboxusercontent.com/s/0i38llqyxfylc2l/2018-01-02%2013.42.18.jpg";
         DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        request.setDescription("Some descrition");
-        request.setTitle("Downloading Reservations");
-        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "my-map.jpg");
+        String[] parts = url.split("/");
+        String basename = parts[parts.length-1];
+        request.setDescription("Datenbank von Reservationen");
+        request.setTitle(basename);
+        request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, basename);
 
         DownloadManager manager = (DownloadManager) activity.getSystemService(Context.DOWNLOAD_SERVICE);
         manager.enqueue(request);
