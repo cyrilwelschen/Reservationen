251245d6599cd1ef97823d0e44cb2ac77d63cf13
cyrilwelschen <cj.welschen@gmail.com>
2018-06-28 22:21:13 +0200

    Working list widget.

---
+++ app/src/main/java/com/example/cyrilwelschen/reservationen/MainWidget.java
@@ -0,0 +1,57 @@
+package com.example.cyrilwelschen.reservationen;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+/**
+ * Created by cyril on 28.06.18.
+ * Implementation of App Widget functionality.
+ */
+
+public class MainWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+//        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Set up the collection
+        setRemoteAdapter(context, views);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    /**
+     * Sets the remote adapter used to fill in the list items
+     *
+     * @param views RemoteViews to set the RemoteAdapter
+     */
+    //@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    private static void setRemoteAdapter(Context context, @NonNull final RemoteViews views) {
+        views.setRemoteAdapter(R.id.widget_list,
+                new Intent(context, WidgetService.class));
+    }
+}
