7d37eec2b09a120c337e4be2774981e460e8ed15
cyrilwelschen <cj.welschen@gmail.com>
2018-07-06 20:22:18 +0200

    Date row okay.

--- app/src/main/java/com/example/cyrilwelschen/reservationen/MainWidget.java
+++ app/src/main/java/com/example/cyrilwelschen/reservationen/MainWidget.java
@@ -1,10 +1,16 @@
 package com.example.cyrilwelschen.reservationen;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.view.View;
 import android.widget.RemoteViews;
+
+import java.util.Date;
 
 /**
  * Created by cyril on 28.06.18.
@@ -23,17 +29,51 @@ public class MainWidget extends AppWidgetProvider {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+    /*
+    private String intDiffToDateString(int intDayDiff){
+        Calendar todayCal = Calendar.getInstance();
+        todayCal.add(Calendar.DATE, intDayDiff);
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd.MMM");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("cccc");
+        return sdf1.format(todayCal) + "\n" + sdf2.format(todayCal);
+    }
+    */
+
+    private String intDiffToDateString(int intDayDiff){
+        Calendar todayCal = Calendar.getInstance();
+        todayCal.add(Calendar.DATE, intDayDiff);
+        SimpleDateFormat sdf1 = new SimpleDateFormat("ccc dd.MMM");
+        return sdf1.format(todayCal);
+    }
+
+    void setDates(RemoteViews views){
+        views.setTextViewText(R.id.date_row1, intDiffToDateString(-2));
+        views.setTextViewText(R.id.date_row2, intDiffToDateString(-1));
+        views.setTextViewText(R.id.date_row3, intDiffToDateString(0));
+        views.setTextViewText(R.id.date_row4, intDiffToDateString(1));
+        views.setTextViewText(R.id.date_row5, intDiffToDateString(2));
+
+    }
+
+    void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+        views.setOnClickPendingIntent(R.id.relative_layout, pendingIntent);
+
+        setDates(views);
 
         views.setViewVisibility(R.id.r30036, View.VISIBLE);
         views.setTextViewText(R.id.r30036, "test1");
         views.setViewVisibility(R.id.r30013, View.VISIBLE);
         views.setTextViewText(R.id.r30013, "test1");
-        views.setViewVisibility(R.id.r30214, View.VISIBLE);
+        views.setViewVisibility(R.id.r30114, View.VISIBLE);
         views.setTextViewText(R.id.r30214, "test2");
+        views.setViewVisibility(R.id.r30706, View.VISIBLE);
+        views.setTextViewText(R.id.r30706, "hello");
         views.setViewVisibility(R.id.r30806, View.VISIBLE);
-        views.setTextViewText(R.id.r30806, "test2");
+        views.setTextViewText(R.id.r30806, "you");
 
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
